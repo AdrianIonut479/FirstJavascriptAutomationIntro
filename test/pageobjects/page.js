9b48eea33bf85b0b123603383cad8d7292cf875d
AdrianIonut479 <59884250+AdrianIonut479@users.noreply.github.com>
2021-04-24 20:28:07 +0300

    Delete test/pageobjects directory

--- test/pageobjects/page.js
+++
@@ -1,5 +0,0 @@
-module.exports = class Page{
-    open() {
-        return browser.url('https://www.digi24.ro/')
-    }
-}
--- test/pageobjects/pageMagazin.js
+++
@@ -1,5 +0,0 @@
-module.exports = class PageMagazin{
-    open() {
-        return browser.url('https://www.digi24.ro/magazin/stil-de-viata')
-    }
-}
